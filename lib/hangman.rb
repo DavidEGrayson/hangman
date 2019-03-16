89889fffa83d3e39d34f1f4173f1e3bb94554f3e
David Grayson <davidegrayson@gmail.com>
2019-03-15 21:00:42 -0700

    Try to make make_guess iterate through the whole tree, but it is too slow.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -11,15 +11,20 @@ class Hangman
     words.grep regexp
   end
 
+  def generate_pattern(word, pattern, guess)
+    p = pattern.dup
+    word.length.times do |i|
+      if word[i] == guess
+        p[i] = guess
+      end
+    end
+    p
+  end
+
   def get_options(words, pattern, guess)
     options = {}
     words.each do |word|
-      p = pattern.dup
-      word.length.times do |i|
-        if word[i] == guess
-          p[i] = guess
-        end
-      end
+      p = generate_pattern(word, pattern, guess)
       options[p] ||= 0
       options[p] += 1
     end
@@ -71,15 +76,51 @@ class Hangman
   end
 
   def make_guess(words, pattern, used_letters)
+    make_guess_core(words, pattern, used_letters)[0]
+  end
+
+  # Returns the guess we should make, and the number of
+  # incorrect guesses remaining before victory.
+  def make_guess_core(words, pattern, used_letters)
     unused_letters = Letters - used_letters
 
     guesses = {}
     unused_letters.each do |guess|
-      options = get_options(words, pattern, guess)
-      opponent_option = best_option(options)
-      guesses[guess] = options[opponent_option]
+      incorrect_guess_count = 0
+
+      options = {}
+      words.each do |word|
+        p = generate_pattern(word, pattern, guess)
+        options[p] ||= []
+        options[p] << word
+      end
+      opponent_choice, remaining_words = options.max_by { |k, v| v.size }
+
+      if opponent_choice == pattern
+        incorrect_guess_count += 1
+
+        if remaining_words.size == words.size
+          # This guess has no value.
+          #puts "Bad guess: #{pattern} #{guess} #{words.size}"
+          next
+        end
+      end
+
+      if opponent_choice.include?('_')
+        r = make_guess_core(remaining_words, opponent_choice, used_letters + [guess])
+        incorrect_guess_count += r[1]
+      end
+
+      guesses[guess] = incorrect_guess_count
+
+      puts "-#{used_letters.join} #{guess} => #{incorrect_guess_count}"
+
+      if incorrect_guess_count == 0
+        # Can't do better than this guess.
+        break
+      end
     end
 
-    guesses.min_by { |k, v| v }.first
+    guesses.min_by { |k, v| v } or raise "All guesses are bad #{pattern} #{used_letters.join}"
   end
 end
